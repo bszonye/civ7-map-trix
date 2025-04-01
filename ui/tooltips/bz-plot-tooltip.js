2b40ea7955c321635799fbbdb4ef19ba6f0464ab
B. Szonye <bszonye@gmail.com>
2025-04-01 09:07:55 -0700

    change warehouses to 75% opacity

--- ui/tooltips/bz-plot-tooltip.js
+++ ui/tooltips/bz-plot-tooltip.js
@@ -265,7 +265,7 @@ function constructibleColors(info) {
     if (!base.length && !bonus.length) return null;  // walls
     if (!base.length) return bonus;  // no current examples of this
     // warehouse & infrastructure buildings: darken the base yield
-    if (!bonus.length) return [base.at(-1) + "bb", ageless];
+    if (!bonus.length) return [base.at(-1) + "c0", ageless];
     // when possible, select different colors
     const cbase = base.at(-1);  // favor influence & happiness yields
     const cbonus = bonus.at(-1);
