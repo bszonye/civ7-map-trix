e65e2acb6195bd7dcaa934f801ff719e961982cd
B. Szonye <bszonye@gmail.com>
2025-10-01 16:55:21 -0700

    allow lenses to inherit layers

--- ui/lenses/lens/bz-commander-lens.js
+++ ui/lenses/lens/bz-commander-lens.js
@@ -1,19 +1,18 @@
 import { L as LensManager } from '/core/ui/lenses/lens-manager.chunk.js';
 class bzCommanderLens {
-    constructor() {
-        this.activeLayers = new Set([
-            "fxs-operation-target-layer",
-            "bz-discovery-layer",
-            "bz-fortification-layer",
-            "bz-route-layer",
-            "bz-terrain-layer",
-        ]);
-        this.allowedLayers = new Set([
-            "fxs-hexgrid-layer",
-            "fxs-resource-layer",
-            "bz-city-borders-layer",
-            "bz-culture-borders-layer",
-        ]);
-    }
+    activeLayers = new Set([
+        "fxs-operation-target-layer",
+        "bz-culture-borders-layer",
+        "bz-discovery-layer",
+        "bz-fortification-layer",
+        "bz-route-layer",
+        "bz-terrain-layer",
+    ]);
+    allowedLayers = new Set([
+        "fxs-hexgrid-layer",
+        "fxs-resource-layer",
+        "bz-city-borders-layer",
+    ]);
+    blendEnabledLayersOnTransition = false;
 }
 LensManager.registerLens("bz-commander-lens", new bzCommanderLens());
--- ui/lenses/lens/bz-religion-lens.js
+++ ui/lenses/lens/bz-religion-lens.js
@@ -1,16 +1,17 @@
 import { L as LensManager } from '/core/ui/lenses/lens-manager.chunk.js';
 class bzReligionLens {
-    constructor() {
-        this.activeLayers = new Set([
-            "fxs-operation-target-layer",
-            "bz-city-borders-layer",
-            "bz-discovery-layer",
-            "bz-religion-layer",
-        ]);
-        this.allowedLayers = new Set([
-            "fxs-hexgrid-layer",
-            "fxs-resource-layer",
-        ]);
-    }
+    activeLayers = new Set([
+        "fxs-operation-target-layer",
+        "bz-city-borders-layer",
+        "bz-discovery-layer",
+        "bz-religion-layer",
+    ]);
+    allowedLayers = new Set([
+        "fxs-hexgrid-layer",
+        "fxs-resource-layer",
+        "bz-route-layer",
+        "bz-terrain-layer",
+    ]);
+    blendEnabledLayersOnTransition = false;
 }
 LensManager.registerLens("bz-religion-lens", new bzReligionLens());
