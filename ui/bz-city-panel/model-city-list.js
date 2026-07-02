84544ebec6fd5aabfb4f43057029d181c31d7d48
B. Szonye <bszonye@gmail.com>
2026-07-01 18:10:14 -0700

    better conditional ordering

--- ui/bz-city-panel/model-city-list.js
+++ ui/bz-city-panel/model-city-list.js
@@ -93,7 +93,7 @@ class bzCityListModel {
         this._cityList = this._settlementList.filter(c => !c.isTown);
         this._townList = this._settlementList.filter(c => c.isTown);
         // mark subhead dividers for Distant Lands cities and towns
-        const cityDL = this._cityList.find(s => !s.isCapital && s.isDistantLands);
+        const cityDL = this._cityList.find(s => s.isDistantLands && !s.isCapital);
         if (cityDL && cityDL !== this._cityList[1] && cityDL !== this._cityList.at(-1)) {
             cityDL.subhead = "LOC_PLOT_TOOLTIP_HEMISPHERE_WEST";
         }
